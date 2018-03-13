efee871d6e1357c2b24df3bab9c8f2c72e836332
Francisco Rabasco Diaz Chiron <franciscorabascodc@hotmail.com>
2018-03-13 17:29:17 +0100

    Model añadido y REST controller iniciado

---
+++ Ejhercicio1/Controller/RestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.IO;
+
+
+namespace Ejhercicio1.Controller
+{
+    class RestControllers
+    {
+        [HttpPost]
+        public ActionResult getUserById(string Id)
+        {
+            string receivedData;
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest
+                .Create("http://www.mocky.io/v2/5808862710000087232b75ac");
+
+            req.ContentType = "application/json";
+            var response = (HttpWebResponse)req.GetResponse();
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                receivedData = sr.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ Ejhercicio1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Net;
+using System.Configuration;
+using Newtonsoft.Json;
+using Ejhercicio1.Model;
+
+namespace Ejhercicio1s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileStr = ConfigurationManager.AppSettings["file"];
+            var jsonStr = ConfigurationManager.AppSettings["jsonSrc"];
+            //Obtenemos Json
+            var json = new WebClient().DownloadString(jsonStr);
+            var data = JsonConvert.DeserializeObject<User.RootObject>(json);
+
+
+
+            //Creamos ficheros
+            string outCsvFile = string.Format(fileStr);
+
+            //Libera de memoria el objeto stream
+            using (var stream = File.CreateText(outCsvFile))
+            {
+                stream.WriteLine(json);
+                //siempre tienes que cerrar
+                stream.Close();
+               
+            }
+
+
+            //Antigua forma
+            //try
+            //{
+
+            //}
+            //finally
+            //{
+            //    stream.Dispose();
+            //}
+          
+
+
+        }
+    }
+
+
+}
